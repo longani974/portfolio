1b366f7bfdf793ffa4ae4974e1c8fb93497429c5
Sylvain Rivière <sylv1.riviere@gmail.com>
2021-04-09 11:27:38 +0400

    add: a propos

---
+++ pages/a-propos/index.js
@@ -0,0 +1,59 @@
+import Head from 'next/head';
+import Button from '../../components/ui/button/button';
+import styles from './styles.module.css';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+
+export default function APropos() {
+    const router = useRouter();
+
+    return (
+        <>
+            <Head>
+                <title>Portfolio de Sylvain Rivière</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className="innerWrapper">
+                <main>
+                    <h3>La question existentiel</h3>
+                    <h1 className={styles.h1Important}>Qui suis-je?</h1>
+                    <div className={styles.container}>
+                        <div className={styles.contenu}>
+                            <p>
+                                Je suis Sylvain Rivière, développerur web
+                                autodidacte spécialisé dans le front-end.{' '}
+                            </p>
+                            <p>
+                                J’ai commencer à m’intéresser au code par simple
+                                curiosité fin 2019, mais très vite j’ai été pris
+                                par la passion. En recherche de reconversion
+                                professionnel, j’ai tout de suite compris que je
+                                devais creuser cette piste.
+                            </p>
+                            <p>
+                                Depuis j’ai appris énormément, et je ne suis
+                                satisfait de mon code que lorsque le rendu final
+                                correspond exactement à la maquette et au cahier
+                                des charges.
+                            </p>
+                            <p>
+                                Je code essentielement en html, css, JavaScript,
+                                reactJS et le framwork nextJS.
+                            </p>
+                            <a onClick={() => router.push('/contact')}>
+                                <Button>Me contacter</Button>
+                            </a>
+                        </div>
+                        <div className={styles.portrait}>
+                            <Image
+                                src="/images/portrait.png"
+                                width={400}
+                                height={400}
+                            />
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </>
+    );
+}
