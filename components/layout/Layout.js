852c52f2a9c9c7db77c7751ee1a5d64b98ef73d4
Sylvain Rivière <sylv1.riviere@gmail.com>
2021-04-09 16:13:49 +0400

    add: footer

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,10 +1,12 @@
 import Navigation from '../header/Header';
+import Footer from '../ui/footer/Footer';
 
 function Layout(props) {
     return (
         <>
             <Navigation />
             {props.children}
+            <Footer />
         </>
     );
 }
---
+++ components/ui/footer/Footer.js
@@ -0,0 +1,11 @@
+import styles from './footer.module.css';
+
+function Footer() {
+    return (
+        <div className={styles.footer}>
+            Code and Design by Sylvain Rivère - copyright 2021
+        </div>
+    );
+}
+
+export default Footer;
