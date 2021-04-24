92ceff1bb580ab2615ce22948735976029caa79e
Sylvain Rivière <sylv1.riviere@gmail.com>
2021-04-24 20:46:32 +0400

    fix: page index: footer flottant sur certains navigateurs (remplacement de aspect-radio par padding-top)

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -2,13 +2,13 @@ import Navigation from '../header/Header';
 import Footer from '../ui/footer/Footer';
 
 function Layout(props) {
-    return (
-        <>
-            <Navigation />
-            {props.children}
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Navigation />
+      {props.children}
+      <Footer />
+    </>
+  );
 }
 
 export default Layout;
