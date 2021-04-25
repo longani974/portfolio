e9970f36c6083c73922a15a5c9e3bf487cd629de
Sylvain Rivière <sylv1.riviere@gmail.com>
2021-04-25 08:31:59 +0400

    fix: typo dans le footer

--- components/ui/footer/Footer.js
+++ components/ui/footer/Footer.js
@@ -3,7 +3,7 @@ import styles from './footer.module.css';
 function Footer() {
   return (
     <div className={styles.footer}>
-      Coded and Designed by Sylvain Rivère - copyright 2021
+      Coded and Designed by Sylvain Rivière - copyright 2021
     </div>
   );
 }
