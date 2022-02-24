22af10ac877a7bcab6404729d7273b09ae09f079
Sylvain Rivière <sylv1.riviere@gmail.com>
2022-02-24 09:56:49 +0400

    shared nodemailer

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,33 +1,35 @@
 import nodemailer from 'nodemailer';
 
 export default async (req, res) => {
-    const { email, titre, message } = req.body;
+  const { email, titre, message, subject } = req.body;
 
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-            user: process.env.USER_GMAIL,
-            pass: process.env.PASS_GMAIL,
-        },
-    });
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.USER_GMAIL,
+      pass: process.env.PASS_GMAIL,
+    },
+  });
 
-    try {
-        const emailResponse = await transporter.sendMail({
-            from: email,
-            to: process.env.USER_GMAIL,
-            subject: `Formulaire du portfolio - ${titre}`,
-            html: `<p>Message en provenance du portfolio</p><br>
+  try {
+    const emailResponse = await transporter.sendMail({
+      from: email,
+      to: process.env.USER_GMAIL,
+      subject: `${
+        subject ? subject : 'Formulaire du portfolio'
+      }Formulaire du portfolio - ${titre}`,
+      html: `<p>Message en provenance du portfolio</p><br>
          <p>${titre}<br></p>
          <p>${message}<br></p>
          <p>${email}<br></p>`,
-        });
+    });
 
-        console.log('Message envoyez', emailResponse.messageId);
-    } catch (error) {
-        console.log(error);
-    }
+    console.log('Message envoyez', emailResponse.messageId);
+  } catch (error) {
+    console.log(error);
+  }
 
-    res.status(200).json(req.body);
+  res.status(200).json(req.body);
 };
